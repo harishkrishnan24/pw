a18e20ab69038f3d7a6400f74c80b823c2458826
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-03-25 10:23:16 +0530

    Added more listeners

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  length = 0;
   password = "";
   includeLetters = false;
   includeNumbers = false;
@@ -25,5 +26,12 @@ export class AppComponent {
 
   onChangeUseSymbols() {
     this.includeSymbols = !this.includeSymbols;
+  }
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
   }
 }
