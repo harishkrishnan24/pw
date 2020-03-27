cb5316a01c880c32816050f1b2d07e4fce201aff
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-03-27 16:12:08 +0530

    Deployed to now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,27 @@ export class AppComponent {
   includeSymbols = false;
 
   onButtonClick() {
-    this.password = "my password";
+    const numbers = "1234567890";
+    const letters = "abcdefghijklmnopqrstuvwxyz";
+    const symbols = "!@#$%^&*()";
+
+    let validChars = "";
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = "";
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
   }
 
   onChangeUseLetters() {
